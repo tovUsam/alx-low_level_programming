f969d552c52259e912dae338d5fbe67cf37fec89
tovUsam <tovie.oba@gmail.com>
2022-06-07 06:00:10 -0700

    nested

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,19 +1,22 @@
 #include "main.h"
+#include <string.h>
 
 /**
- * main -  prints _putchar, followed by a new line
- * Return: Always 0 (Success)
+ * main - prints holberton
+ * Return: Always (0)
  */
 int main(void)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
+	char c[] = "_putchar";
+	int length, i;
+
+	length = strlen(c);
+
+	for (i = 0; i < length; i++)
+	{
+		_putchar(c[i]);
+	}
+
 	_putchar('\n');
 
 	return (0);
